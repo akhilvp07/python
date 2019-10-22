6447bf984cc65e3e1fd73d7a25bf6910ba290369
Akhil Valsamrithan <avalsamrithan@rbbn.com>
2019-10-22 16:51:11 +0530

    New files

---
+++ src/averageMarks.py
@@ -0,0 +1,15 @@
+print("Average marks calculator")
+
+phy=int(input("Enter marks for Physics :"))
+che=int(input("Enter marks for Chemistry :"))
+maths=int(input("Enter marks for Maths :"))
+
+avg = (phy + che + maths)/3
+
+print("Average marks :{}".format(avg)) 
+if(avg>=80):
+    print("Eligible with distinction!")
+elif(avg >= 35):
+    print("Eligible")
+else:
+    print("Not eligible")
---
+++ src/basic_input.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+print("Hello", name, "! Welcome !")
+
+age = int(input("Please enter your age:"))
+
+if(age<18):
+    print("You are not eligible to vote.")
+else:
+    print("You are eligible to vote.")
